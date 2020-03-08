ba81b2658094d7c0541da7dbe7bf19a1e083053c
Paige Scott <paige.scott@stitchfix.com>
2020-03-08 15:30:38 -0700

    Allow decrementing spells, sorta

--- src/App.js
+++ src/App.js
@@ -2,47 +2,56 @@ import React, { useState, useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const fetch = [4, 3, 1] // This will be where we grab from the API later
-const spells = {
-  spellSlots: fetch.reverse(),
-  setSpellSlots: () => {
-    console.log("BOOM!")
-  }
+let fetch = [4, 3, 2, 1, 0, 0, 0, 0, 0].reverse(); // This will be where we grab from the API later
+
+function blah (position) {
+  console.log("BOOM!", position);
+  spells.spellSlots[position] = spells.spellSlots[position] - 1;
+  console.log(spells.spellSlots);
 }
-const SpellSlotContext = React.createContext(spells)
+
+let spells = {
+  spellSlots: fetch, // Remember this is reversed!
+  setSpellSlots: blah
+}
+
+const SpellSlotContext = React.createContext()
 
 function Pyramid() {
   const context = useContext(SpellSlotContext)
+  const [spellSlots, setSpellSlots] = useState(context.spellSlots)
+
+  const levelLabels = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth"].reverse();
 
   return (
-    <div style={{margin: '10px', border: '1px solid'}}>
-      {context.spellSlots.map((slots, index) => {
-        return <Row slots={slots} key={index} />
+    <div style={{margin: '10px'}}>
+      {spellSlots.map((slots, index) => {
+        return <Row slots={slots} label={levelLabels[index]} position={index} key={index} />
       })}
     </div>
   )
 }
 
-function Row({ slots }) {
+function Row({ slots, label, position }) {
   const context = useContext(SpellSlotContext)
 
   let checkboxes = [];
   for (let index = 0; index < slots; index++) {
-    checkboxes.push(<Checkbox checked={true} key={index}/>)
+    checkboxes.push(<Checkbox checked={true} key={index} position={position}/>)
   }
 
   return(
     <div style={{marginTop: '10px', border: '1px solid'}}>
-      {checkboxes}
+      <label>{label}:</label> {checkboxes}
     </div>
   )
 }
 
-function Checkbox({ checked }) {
+function Checkbox({ checked, position }) {
   const context = useContext(SpellSlotContext)
 
   function handleChange() {
-    context.setSpellSlots()
+    context.setSpellSlots(position)
   }
 
   return <input type='checkbox' defaultChecked={checked} onClick={handleChange}></input>
