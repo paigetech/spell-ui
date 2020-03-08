17dff62d4853e896da688e1bcf6d289ddc3df519
Paige Scott <paige.scott@stitchfix.com>
2020-03-08 14:25:14 -0700

    Make some checkboxes appear

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const blah = {
+  spellSlots: [true, true, false],
+  setSpellSlots: () => {
+    console.log("BOOM!")
+  }
+}
+const SpellSlotContext = React.createContext(blah)
+
+function Blah() {
+  const context = useContext(SpellSlotContext)
+  const [gotCheckUpdate, setGotCheckUpdate] = useState('Not yet...')
+  console.log("SPELL SLOTS", context);
+  return(
+    <div style={{marginTop: '10px', border: '1px solid'}}>
+      {context.spellSlots.map((slotFilled, index) => {
+        return <Checkbox checked={slotFilled} key={index}/>
+      })}
+      <div>{gotCheckUpdate}</div>
+    </div>
+  )
+}
+
+function Checkbox(props) {
+  const [isChecked, setIsChecked] = useState(props.checked)
+
+  return <input type='checkbox' defaultChecked={isChecked}></input>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SpellSlotContext.Provider value={blah}>
+        <Blah />
+      </SpellSlotContext.Provider>
     </div>
   );
 }
